114bafddc8d0f6c1b28f4a0a434baa880a2e4577
EdenEast <edenofest@gmail.com>
2023-09-07 09:57:31 -0400

    fix(nix): move from nixos-unstable to nixpkgs-unstable

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils = {
       url = "github:numtide/flake-utils";
       inputs.nixpkgs.follows = "nixpkgs";
