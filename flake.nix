91b3c7cb4764b2414291c7d844269efc8a033864
EdenEast <edenofest@gmail.com>
2024-06-03 10:00:31 -0400

    chore(nix): change nixpkgs to 24.05 release

--- flake.nix
+++ flake.nix
@@ -1,21 +1,16 @@
 {
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     flake-utils = {
       url = "github:numtide/flake-utils";
-      inputs.nixpkgs.follows = "nixpkgs";
     };
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-utils.follows = "flake-utils";
     };
     crane = {
       url = "github:ipetkov/crane";
       inputs.nixpkgs.follows = "nixpkgs";
-      inputs.flake-utils.follows = "flake-utils";
-      inputs.flake-compat.follows = "";
-      inputs.rust-overlay.follows = "";
     };
   };
 
